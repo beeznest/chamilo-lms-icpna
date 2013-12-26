5980d8d671f04f79e279970ca29296a709f5e7a9
Yannick Warnier <ywarnier@beeznest.org>
2013-12-25 23:30:33 -0500

    Added migrate dir for all things related to migration, excluding config files and branches UUIDs

---
+++ tests/migrate/db_matches_aula.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * This script defines as closely as possible the matches between
+ * original tables and destination (chamilo) tables and fields.
+ * See db_matches.dist.php for formatting documentation
+ */
+
+//intIdTipoEmpleado = 2  = Profesor
+// This is an array that matches objects (for Chamilo)
+$matches = array(
+  
+    //Sessions
+    /* Debería ser 63759 con la última data del servidor de prueba */
+    array(
+        'orig_table' => 'ProgramaAcademico',
+        'query' => 'SELECT %s, SUBSTRING(s.vchNombreSede,14,12) + \' - \' + chrPeriodo + \' - \' +vchNombreCurso + \' \' + chrHoraInicial + \' \' + chrHoraFinal +  \' \' + a.vchIdentificadorFisico as session_name '.
+                   ' FROM ProgramaAcademico p '.
+                   ' INNER JOIN Curso c ON p.uidIdCurso = c.uidIdCurso '.
+                   ' INNER JOIN Horario h ON h.uidIdHorario = p.uidIdHorario '.
+                   ' LEFT JOIN Aula a ON a.uidIdAula = p.uidIdAula '.
+                   ' INNER JOIN Sede s ON (s.uidIdSede = p.uidIdSede AND s.intIdSede = '.$branch.') '.
+                   ' LEFT JOIN Empleado e ON (e.uidIdEmpleado = p.uidIdProfesor) '.
+                   '',
+        'dest_table' => 'session',
+        'dest_func' => 'MigrationCustom::create_session',
+        'order' => 'ORDER BY chrPeriodo',
+        'extra_fields' => array(            
+            /*'uidIdPrograma' => array(                
+                'field_display_text' => 'uidIdPrograma',
+                'field_variable' => 'uidIdPrograma',
+                'field_visible' => '1',
+                'field_type' => ExtraField::FIELD_TYPE_TEXT,                
+            ),*/
+            /*
+            'aula' => array(                
+                'field_display_text' => 'Aula',
+                'field_variable' => 'aula',
+                'field_visible' => '1',
+                'field_type' => ExtraField::FIELD_TYPE_TEXT,                
+            ),*/            
+            'aula' => array(        
+                'field_display_text' => 'Aula',
+                'field_variable' => 'aula',
+                'field_visible' => '1',
+                'field_type' => ExtraField::FIELD_TYPE_SELECT,
+                'options' => array(
+                    'orig_table' => 'Aula',
+                    'query' => 'SELECT %s  FROM Aula ',
+                    'fields_match' => array(
+                        array(
+                            'orig' => 'vchIdentificadorFisico',
+                            'sql_alter' => null,
+                            'dest' => 'option_display_text',
+                            'func' => '',
+                        ),        
+                        array(
+                            'orig' => 'uidIdAula',
+                            'sql_alter' => 'sql_alter_unhash_50',
+                            'dest' => 'option_value',
+                            'func' => null,
+                        ),     
+                    )
+                ),
+            ),
+        ),
+        'fields_match' => array(         
+            array(                
+                'orig' => 'chrPeriodo',
+                'sql_alter' => '',
+                'dest' => 'name',
+                'func' => 'clean_session_name',
+            ),
+            array(
+                'orig' => 'sdtFechaInicioClases',
+                'sql_alter' => '',
+                'dest' => 'display_start_date',
+                'func' => 'none',
+            ),
+            array(
+                'orig' => 'sdtFechaFinClases',
+                'sql_alter' => '',
+                'dest' => 'display_end_date',
+                'func' => 'none',
+            ),
+            array(
+                'orig' => 'sdtFechaInicioClases',
+                'sql_alter' => '',
+                'dest' => 'access_start_date',
+                'func' => 'none',
+            ),
+            array(
+                'orig' => 'sdtFechaFinClases',
+                'sql_alter' => '',
+                'dest' => 'access_end_date',
+                'func' => 'none',
+            ),
+            //Getting info from chamilo
+            array(
+                'orig' => 'p.uidIdCurso',
+                'sql_alter' => 'sql_alter_unhash_50',
+                'dest' => 'course_code',
+                'func' => 'get_real_course_code',
+            ),
+            array(
+                'orig' => 'uidIdPersona',
+                'sql_alter' => 'sql_alter_unhash_50',
+                'dest' => 'id_coach',
+                'func' => 'get_real_teacher_id',
+            ),
+            //Extra fields
+            array(
+                'orig' => 'uidIdPrograma',
+                'sql_alter' => 'sql_alter_unhash_50',
+                'dest' => 'extra_field_uidIdPrograma',
+                'func' => null,
+            ),
+            array (
+                'orig' => 'p.uidIdAula',
+                'sql_alter' => 'sql_alter_unhash_50',
+                'dest' => 'extra_field_aula',
+                'func' => null,
+            ),           
+        ),
+    ),
+);

9a5633b19a0a22b2818a97ee57e7fc52ab8e178b
Julio Montoya <gugli100@gmail.com>
2013-12-27 18:26:45 +0100

    Fixing center col see BT#7205

--- main/inc/lib/course_home.lib.php
+++ main/inc/lib/course_home.lib.php
@@ -672,7 +672,7 @@ class CourseHome {
                         }
                     }
                 } else {
-                    $item['visibility'] .=  '&nbsp;&nbsp;&nbsp;&nbsp;';
+                    $item['visibility'] .= null;
                 }
 
                 // NOTE : Table contains only the image file name, not full path

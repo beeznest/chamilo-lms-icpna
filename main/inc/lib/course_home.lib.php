21dfd89b2d4d7cbaae5f17d3c70092fbe6cbd444
Daniel Barreto <9leinad0@gmail.com>
2014-01-19 16:46:09 -0500

    Fix  regex used when replace src of img in 'pure_icon' - refs BT#7219

--- main/inc/lib/course_home.lib.php
+++ main/inc/lib/course_home.lib.php
@@ -1160,9 +1160,7 @@ class CourseHome {
             if (!isset($tool['icon'])) {
                 continue;
             }
-
             $image = substr($tool['tool']['image'], 0, 4);
-            var_dump($image);
             switch ($image) {
                 case 'scor':
                     $type_entity_id = 1;
@@ -1177,72 +1175,68 @@ class CourseHome {
                     $id = self::get_published_id_from_link($tool['tool']['link'], 'd=');
                     break;
             }
-            require_once api_get_path(LIBRARY_PATH).'sequence.lib.php';
+            if ($type_entity_id > 0 && $id > 0) {
+                require_once api_get_path(LIBRARY_PATH).'sequence.lib.php';
 
-            $row_entity_id = Sequence::get_row_entity_id_by_row_id($type_entity_id, $id, api_get_course_int_id(), api_get_session_id());
-            $state = Sequence::get_state_lp_by_row_entity_id($row_entity_id, api_get_user_id());
-            $courseInfo = api_get_course_info();
-            $regex ='/src=\"(http|https):\/\/([\w]+\.)+[\w]+(\/[\w .\/?%&=]*)?(\.gif|\.png|\.jpg)/';
-            $dir_icons = 'icons/64/';
-            $ext_png = 'png';
-            if (!empty($tool['tool']['custom_icon'])) {
-                $custom_icon = $tool['tool']['custom_icon'];
-            } else {
-                $custom_icon = preg_replace('/(.)+alt=\"/','',$tool['pure_icon']);
-                $custom_icon = preg_replace('/\"(.)+/','',$custom_icon);
-                if ($custom_icon !== $tool['tool']['image']) {
-                    $is_lp_icon = true;
+                $row_entity_id = Sequence::get_row_entity_id_by_row_id($type_entity_id, $id, api_get_course_int_id(), api_get_session_id());
+                $state = Sequence::get_state_lp_by_row_entity_id($row_entity_id, api_get_user_id());
+                $courseInfo = api_get_course_info();
+                $regex ='/src=\"\S*\.(gif|png|jpg)/';
+                $dir_icons = 'icons/64/';
+                $ext_png = 'png';
+                $is_lp_icon = false;
+                if (!empty($tool['tool']['custom_icon'])) {
+                    $custom_icon = $tool['tool']['custom_icon'];
                 } else {
-                    $is_lp_icon = false;
-                }
-            }
-            $basedir = ($is_lp_icon)? '/upload/learning_path/images/':'/upload/course_home_icons';
-            var_dump($custom_icon);
-            if ($state === 'process' || $state === 'completed') {
-                $tool['visibility'] = 1;
-                $iconPathTemp = api_get_path(WEB_COURSE_PATH).$courseInfo['directory'].$basedir.$custom_icon;
-                if (is_file(api_get_path(SYS_COURSE_PATH).$courseInfo['directory'].$basedir.$custom_icon)) {
-                    $icon_path = 'src="'.$iconPathTemp;
-                    var_dump($icon_path);
-                    $tool['pure_icon'] = preg_replace($regex, $icon_path, $tool['pure_icon']);
-                    $tool['icon'] = preg_replace($regex, $icon_path, $tool['icon']);
-                } else {
-                    $icon_path = 'src="' . api_get_path(WEB_IMG_PATH) . $dir_icons . $tool['tool']['image'];
-                    $icon_path = substr($icon_path,0,-3) . $ext_png;
-                    var_dump($icon_path);
-                    $tool['pure_icon'] = preg_replace($regex, $icon_path, $tool['pure_icon']);
-                    $tool['icon'] = preg_replace($regex, $icon_path, $tool['icon']);
-                }
-            } else {
-                $iconPathNATemp = api_get_path(WEB_COURSE_PATH).$courseInfo['directory'].$basedir.$custom_icon;
-                $iconPathNATempSys = api_get_path(SYS_COURSE_PATH).$courseInfo['directory'].$basedir.$custom_icon;
-                $ext = pathinfo($iconPathNATempSys, PATHINFO_EXTENSION);
-                if (strpos($iconPathNATempSys,'_na') === false) {
-                    $iconPathNATempSys = substr($iconPathNATempSys,0,-(strlen($ext)+1)) . '_na.' . $ext_png;
-                }
-                if (is_file($iconPathNATempSys)) {
-                    if (strpos($iconPathNATemp,'_na') === false) {
-                        $iconPathNATemp = substr($iconPathNATemp,0,-(strlen($ext)+1)) . '_na.' . $ext_png;
+                    $custom_icon = preg_replace('/(.)+alt=\"/','',$tool['pure_icon']);
+                    $custom_icon = preg_replace('/\"(.)+/','',$custom_icon);
+                    if ($custom_icon !== $tool['tool']['image']) {
+                        $is_lp_icon = true;
                     }
-                    $icon_path = $iconPathNATemp;
-                    $icon_path = 'src="' . $icon_path;
-                    var_dump($icon_path);
-                    $tool['pure_icon'] = preg_replace($regex, $icon_path,$tool['pure_icon']);
-                    $tool['icon'] = preg_replace($regex, $icon_path,$tool['icon']);
-                } else {
-                    $ext = pathinfo(api_get_path(WEB_IMG_PATH). $dir_icons . $tool['tool']['image'], PATHINFO_EXTENSION);
-                    $default_icon = api_get_path(WEB_IMG_PATH). $dir_icons . substr($tool['tool']['image'],0,-(strlen($ext)+1));
-                    if (strpos($default_icon,'_na') === false) {
-                        $icon_path = $default_icon .'_na.'.$ext_png;
+                }
+                $basedir = ($is_lp_icon)? '/upload/learning_path/images/':'/upload/course_home_icons/';
+                if ($state === 'process' || $state === 'completed') {
+                    $tool['visibility'] = 1;
+                    $iconPathTemp = api_get_path(WEB_COURSE_PATH).$courseInfo['directory'].$basedir.$custom_icon;
+                    if (is_file(api_get_path(SYS_COURSE_PATH).$courseInfo['directory'].$basedir.$custom_icon)) {
+                        $icon_path = 'src="'.$iconPathTemp;
+                        $tool['pure_icon'] = preg_replace($regex, $icon_path, $tool['pure_icon']);
+                        $tool['icon'] = preg_replace($regex, $icon_path, $tool['icon']);
                     } else {
-                        $icon_path = $default_icon .'.'. $ext_png;
+                        $icon_path = 'src="' . api_get_path(WEB_IMG_PATH) . $dir_icons . $tool['tool']['image'];
+                        $icon_path = substr($icon_path,0,-3) . $ext_png;
+                        $tool['pure_icon'] = preg_replace($regex, $icon_path, $tool['pure_icon']);
+                        $tool['icon'] = preg_replace($regex, $icon_path, $tool['icon']);
                     }
-                    $icon_path = 'src="'. $icon_path;
-                    $tool['pure_icon'] = preg_replace($regex, $icon_path,$tool['pure_icon']);
-                    $tool['icon'] = preg_replace($regex, $icon_path,$tool['icon']);
+                } else {
+                    $iconPathNATemp = api_get_path(WEB_COURSE_PATH).$courseInfo['directory'].$basedir.$custom_icon;
+                    $iconPathNATempSys = api_get_path(SYS_COURSE_PATH).$courseInfo['directory'].$basedir.$custom_icon;
+                    $ext = pathinfo($iconPathNATempSys, PATHINFO_EXTENSION);
+                    if (strpos($iconPathNATempSys,'_na') === false) {
+                        $iconPathNATempSys = substr($iconPathNATempSys,0,-(strlen($ext)+1)) . '_na.' . $ext_png;
+                    }
+                    if (is_file($iconPathNATempSys)) {
+                        if (strpos($iconPathNATemp,'_na') === false) {
+                            $iconPathNATemp = substr($iconPathNATemp,0,-(strlen($ext)+1)) . '_na.' . $ext_png;
+                        }
+                        $icon_path = $iconPathNATemp;
+                        $icon_path = 'src="' . $icon_path;
+                        $tool['pure_icon'] = preg_replace($regex, $icon_path,$tool['pure_icon']);
+                        $tool['icon'] = preg_replace($regex, $icon_path,$tool['icon']);
+                    } else {
+                        $ext = pathinfo(api_get_path(WEB_IMG_PATH). $dir_icons . $tool['tool']['image'], PATHINFO_EXTENSION);
+                        $default_icon = api_get_path(WEB_IMG_PATH). $dir_icons . substr($tool['tool']['image'],0,-(strlen($ext)+1));
+                        if (strpos($default_icon,'_na') === false) {
+                            $icon_path = $default_icon .'_na.'. $ext_png;
+                        } else {
+                            $icon_path = $default_icon .'.'. $ext_png;
+                        }
+                        $icon_path = 'src="'. $icon_path;
+                        $tool['pure_icon'] = preg_replace($regex, $icon_path,$tool['pure_icon']);
+                        $tool['icon'] = preg_replace($regex, $icon_path,$tool['icon']);
+                    }
                 }
             }
-            var_dump($tool);
             $toolName = $tool['tool']['name'];
             $show = '<div class="span2 center">'
 

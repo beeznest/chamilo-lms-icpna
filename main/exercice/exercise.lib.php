65443831fdee39eae04e7a67a72e9b963fa19e60
Francis Gonzales <francis.gonzales@beeznest.com>
2014-05-05 15:24:28 -0500

    Avoid validations by MaxAttemp - refs BT#8054

--- main/exercice/exercise.lib.php
+++ main/exercice/exercise.lib.php
@@ -2767,75 +2767,35 @@ function getFinalScore($cid, $sid)
     // first was not finished (track_e_exercices.status != ''), we have to
     // take the results from the second attempt (but not more)
 
-    $score = 0;
-    
-    if ($maxAttempt == 1) {
-        // Adults case, only one attempt but if first unfinished, we take the
-        // second one
-        $sql = "SELECT exe_result, exe_weighting, status
+    $sql = "SELECT exe_result, exe_weighting, status
             FROM $tbl_res
             WHERE
                 exe_exo_id = $qid AND
                 exe_cours_id = '$ccode' AND
-                session_id = $sid
+                session_id = $sid AND
+                status = ''
             ORDER BY start_date ASC LIMIT 2";
-        $res = Database::query($sql);
-        if (Database::num_rows($res) < 1) {
-            return false;
-        } elseif (Database::num_rows($res) == 1) {
-            $row = Database::fetch_row($res);
-            $tempScore = round(($row[0] / $row[1]) * 100, 0);
-            if ($tempScore < 70 && $row[2] != '') {
-                // return empty array so this score is not taken into account
-                return false;
-            }
-            $score = $tempScore;
-        } else {
-            $lastScore = 0;
-            // only scan 2 rows, thanks to the LIMIT 2 above
-            while ($row = Database::fetch_row($res)) {
-                $tempScore = round(($row[0] / $row[1]) * 100, 0);
-                if ($tempScore > $lastScore) {
-                    $lastScore = $tempScore;
-                }
-            }
-            // return the best score
-            $score = $lastScore;
-        }
+    
+    $res = Database::query($sql);
+    $numRows = Database::num_rows($res);
+    
+    if ($numRows < 1) {
+        return false;
     } else {
-        // There are 3 attempts to these tests. As soon as one is > 70, send result
-        $sql = "SELECT exe_result, exe_weighting, status
-            FROM $tbl_res
-            WHERE exe_exo_id = $qid
-                AND exe_cours_id = '$ccode'
-                AND session_id = $sid
-            ORDER BY start_date ASC LIMIT 3";
-        $res = Database::query($sql);
-        if (Database::num_rows($res) < 1) {
-            return false;
-        }
         $lastScore = 0;
-        $count = 0;
-        // only scan max 3 rows, thanks to the LIMIT 2 above
+        $minPercentage = ($maxAttempt === 3) ? 80 : 70; //PLEX for kids
         while ($row = Database::fetch_row($res)) {
             $tempScore = round(($row[0] / $row[1]) * 100, 0);
             if ($tempScore > $lastScore) {
                 $lastScore = $tempScore;
             }
-            $count++;
         }
-        if ($lastScore >= 70) {
-            // return the success score
-            $score = $lastScore;
+        if ($numRows === $maxAttempt) {
+            return $lastScore;
+        } elseif($lastScore >= $minPercentage) {
+            return $lastScore;
         } else {
-            if ($count == 3) {
-                // reached maximum attempts, return bad result
-                $score = $lastScore;
-            } else {
-                return false;
-            }
+            return false;
         }
     }
-
-    return $score;
 }

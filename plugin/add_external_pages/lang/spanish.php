0bebc7055a525eac5cda826bd97dc49d93c45be6
Francis Gonzales <francis.gonzales@beeznest.com>
2014-06-13 18:26:35 -0500

    Plugin to add external pages in course_home - refs BT#8236

--- plugin/add_external_pages/lang/spanish.php
+++ plugin/add_external_pages/lang/spanish.php
@@ -1,3 +1,4 @@
 <?php
 $strings['plugin_title'] = "Add External Pages to Course";
 $strings['plugin_comment'] = "This plugin is used to show additional buttons in the course with external information embedded.";
+$strings['external_page'] = "External Page";
--- plugin/add_external_pages/src/add_external_pages_plugin.class.php
+++ plugin/add_external_pages/src/add_external_pages_plugin.class.php
@@ -23,8 +23,9 @@ class AddExternalPagesPlugin extends Plugin
     {
         $parameters = array(
             'tool_enable' => 'boolean',
+            'button_name' => 'text',
+            'web_path' => 'text',
             'image_path' =>'text',
-            'button_name' => 'text'
         );
 
         parent::__construct('1.0', 'Francis Gonzales', $parameters);
@@ -32,12 +33,48 @@ class AddExternalPagesPlugin extends Plugin
 
     public function saveAdditionalConfiguration($params)
     {
-        //$session = SessionManager::get_sessions_list();
-        var_dump($params);exit;
+        $toolTable = Database::get_course_table(TABLE_TOOL_LIST);
+        $whereCondition = array(
+                'link like ?' => '%add_page_plugin=1%'
+        );
+        Database::delete($toolTable, $whereCondition);
         if ($params['tool_enable'] == "true") {
+            $imagesPath = explode(';', $params['image_path']);
+            $buttons = explode(';', $params['button_name']);
+            $countButtons = count($buttons);
+            $toolTmp = array();
+            for ($i = 0; $i < $countButtons; $i++) {
+                $toolTmp[] = array(
+                    'name' => $buttons[$i],
+                    'link' => 'plugin/plugin.php?id=' . $i . '&add_page_plugin=1',
+                    'image' => $imagesPath[$i]
+                );
+            }
+            $sql = "SELECT t.* FROM $toolTable t
+                INNER JOIN (
+                    SELECT c_id, MAX(id) as id
+                    FROM $toolTable
+                    GROUP BY c_id
+                ) tmp ON tmp.c_id = t.c_id AND tmp.id = t.id";
 
-        } else {
-
+            $sqlResponse = Database::query($sql);
+            while ($row = Database::fetch_assoc($sqlResponse)) {
+                foreach ($toolTmp as $tool) {
+                    $attributes = array(
+                        'c_id' => $row['c_id'],
+                        'name' => $tool['name'],
+                        'link' => $tool['link'],
+                        'image' => $tool['image'],
+                        'visibility' => 1,
+                        'admin' => 1,
+                        'address' => '',
+                        'added_tool' => 0,
+                        'category' => 'interaction',
+                        'session_id' => $row['session_id']
+                    );
+                    Database::insert($toolTable, $attributes);
+                }
+            }
         }
     }
 }

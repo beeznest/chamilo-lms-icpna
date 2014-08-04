af92efa66587d1de884e03a69d5581a0deabc85e
Yannick Warnier <ywarnier@beeznest.org>
2014-08-04 10:27:55 -0500

    Add documentation to new ws.conf.dist.php - refs BT8489

--- tests/migrate/ws.conf.dist.php
+++ tests/migrate/ws.conf.dist.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Redefine the web_service_calls array initially defined in git-included 
+ * db_matches.php.
+ */
 $matches['web_service_calls'] = array(       
     'url' => "http://***/***/***?wsdl",
     'filename' => 'migration.custom.class.php',

310ec085171df9c6c40fc2703546eda988e651c4
Anibal Copitan <anibal.copitan@beeznest.com>
2014-08-07 18:48:46 -0500

    parameter passing (is_course_plugin)

--- main/inc/lib/plugin.lib.php
+++ main/inc/lib/plugin.lib.php
@@ -314,7 +314,7 @@ class AppPlugin {
                 if (file_exists($plugin_path)) {
                     require_once $plugin_path;
                     if (isset($plugin_info) && isset($plugin_info['plugin_class'])) {
-                        $plugin_info['plugin_class']::create()->course_install($course_id);
+                        $plugin_info['plugin_class']::create()->course_install($course_id, $plugin_info['is_course_plugin']);
                     }
                 }
             }

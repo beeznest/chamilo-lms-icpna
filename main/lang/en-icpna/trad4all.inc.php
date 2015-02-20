11766f7dd437ef7967676c2b76a1dd454a8d982b
aragonc <aragcar@gmail.com>
2015-02-20 14:34:52 -0500

    merge

--- main/lang/en-icpna/trad4all.inc.php
+++ main/lang/en-icpna/trad4all.inc.php
@@ -1,3 +1,7 @@
 <?php
 $langStudentView="Go to Learner View";
 $langCourseManagerview="Go to Teacher View";
+$langMyCourses = "Courses";
+$langMyAgenda = "Agenda";
+$CampusHomepage = "Home";
+$SocialNetwork = "Social";

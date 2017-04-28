f8efc190535ad55bfd010bdc14c80c36662bfcfc
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2017-04-28 14:52:27 -0500

    Disable sso to icpna_tab_zone plugin - refs BT#11549

--- plugin/icpna_tab_zone/src/zone.php
+++ plugin/icpna_tab_zone/src/zone.php
@@ -18,27 +18,27 @@ $isStudent = api_is_student();
 $tabName = $isStudent ? $plugin->get_lang('StudentsZone') : $plugin->get_lang('TeachersZone');
 $zonUrl = $isStudent ? $plugin->get('student_zone_url') : $plugin->get('teacher_zone_url');
 
-$objSsoServer = new ssoServer();
-$objBranch = new Branch();
-
-$sessionId = api_get_session_id();
-$user = api_get_user_info();
-
-// if no session or branch context, pass 0 to login link
-$programUid = empty($sessionId) ? 0 : $objBranch->getUidProgram($sessionId);
-$branchUid = empty($sessionId) ? 0 : $objBranch->getUidSede($sessionId);
-
-$additionalParams = [
-    'profile' => $isStudent ? 'learner' : 'trainer'
-];
-
-$zoneSsoUrl = $objSsoServer->getUrl($zonUrl, $additionalParams);
+//$objSsoServer = new ssoServer();
+//$objBranch = new Branch();
+//
+//$sessionId = api_get_session_id();
+//$user = api_get_user_info();
+//
+//// if no session or branch context, pass 0 to login link
+//$programUid = empty($sessionId) ? 0 : $objBranch->getUidProgram($sessionId);
+//$branchUid = empty($sessionId) ? 0 : $objBranch->getUidSede($sessionId);
+//
+//$additionalParams = [
+//    'profile' => $isStudent ? 'learner' : 'trainer'
+//];
+//
+//$zoneSsoUrl = $objSsoServer->getUrl($zonUrl, $additionalParams);
 
 $htmlHeadXtra[] = '<link rel="stylesheet" href="'.api_get_path(WEB_PLUGIN_PATH).'icpna_tab_zone/views/tab_zone.css">'
     .'</link>';
 
 $objTpl = new Template($tabName);
-$objTpl->assign('path', $zoneSsoUrl);
+$objTpl->assign('path', $zonUrl);
 
 $content = $objTpl->fetch('icpna_tab_zone/views/zone.tpl');
 

ccb3336774c2dee8ea22541f789e2005611c0146
Yannick Warnier <ywarnier@beeznest.org>
2017-06-09 15:12:27 -0500

    Add language terms from previous version - refs BT#12877

---
+++ main/lang/en-icpna/trad4all.inc.php
@@ -0,0 +1,8 @@
+<?php
+$langStudentView="Go to Learner View";
+$langCourseManagerview="Go to Teacher View";
+$langMyCourses = "Courses";
+$langMyAgenda = "Agenda";
+$CampusHomepage = "Home";
+$SocialNetwork = "Social";
+$ShowStudentViewTitle="Learner View";

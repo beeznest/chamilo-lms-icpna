2a4cf02575e86324bfa7851be299265c26679a2a
Yannick Warnier <ywarnier@beeznest.org>
2017-07-12 00:22:59 -0500

    Fix other extra field issue in sessions creation (missing extra_ prefixes) - refs BT#12885

--- tests/migrations/icpna/migration.custom.class.php
+++ tests/migrations/icpna/migration.custom.class.php
@@ -1528,23 +1528,23 @@ class MigrationCustom
             self::fixAccessDates($session_info);
             $session_info['id_coach'] = 0;
             $extra = [];
-            if (!empty($data['uidIdPrograma'])) {
-                $extra['uidIdPrograma'] = $data['uidIdPrograma'];
+            if (!empty($session_info['extra_uidIdPrograma'])) {
+                $extra['extra_uididprograma'] = $session_info['extra_uidIdPrograma'];
             }
-            if (!empty($data['estado'])) {
-                $extra['estado'] = $data['estado'];
+            if (!empty($session_info['estado'])) {
+                $extra['extra_estado'] = $session_info['estado'];
             }
-            if (!empty($data['sede'])) {
-                $extra['sede'] = $data['sede'];
+            if (!empty($session_info['extra_sede'])) {
+                $extra['extra_sede'] = $session_info['extra_sede'];
             }
-            if (!empty($data['horario'])) {
-                $extra['horario'] = $data['horario'];
+            if (!empty($session_info['extra_horario'])) {
+                $extra['extra_horario'] = $session_info['extra_horario'];
             }
-            if (!empty($data['periodo'])) {
-                $extra['periodo'] = $data['periodo'];
+            if (!empty($session_info['extra_periodo'])) {
+                $extra['extra_periodo'] = $session_info['extra_periodo'];
             }
-            if (!empty($data['aula'])) {
-                $extra['aula'] = $data['aula'];
+            if (!empty($session_info['extra_aula'])) {
+                $extra['extra_aula'] = $session_info['extra_aula'];
             }
             $session_id = SessionManager::create_session(
                 $session_info['name'],

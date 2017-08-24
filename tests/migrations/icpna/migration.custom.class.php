cfa42b9953310144ac80d5d81dbd4e2f0403790b
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2017-08-24 11:57:19 -0500

    Avoid show teacher subscribed as student in session - refs BT#13232

--- tests/migrations/icpna/migration.custom.class.php
+++ tests/migrations/icpna/migration.custom.class.php
@@ -1354,12 +1354,6 @@ class MigrationCustom
             if (!empty($session_id)) {
                 $before = self::getUserStatusInSession($session_id, $user_id);
                 if (isset($status) && $status == 1) {
-                    SessionManager::subscribe_users_to_session(
-                        $session_id,
-                        array($user_id),
-                        SESSION_VISIBLE_READ_ONLY,
-                        false
-                    );
                     $course_list = SessionManager::get_course_list_by_session_id($session_id);
                     if (!empty($course_list)) {
                         $course_data = current($course_list);

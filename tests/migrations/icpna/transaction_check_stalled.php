05ac3ca65b660d3d3fb62eacc1f0d149d8246891
Yannick Warnier <ywarnier@beeznest.org>
2017-08-28 19:32:35 +0200

    Add debug method and debug stop points - refs BT#13264

--- tests/migrations/icpna/transaction_check_stalled.php
+++ tests/migrations/icpna/transaction_check_stalled.php
@@ -14,31 +14,46 @@ $pid_file = __DIR__.'/chamilo.transaction.pid';
 $pid_fix_file = __DIR__.'/chamilo.transaction.fix.pid';
 $now = time();
 $failing = false;
+$debug = false;
 /**
  * Checks
  */
 // check if the normal transactions pid file is present
 if (is_file($pid_file)) {
+    logOnDebug($debug, 'File '.$pid_file.' exists (process running?)');
     //read contents of last transaction run log
     if (is_file($last_file)) {
+        logOnDebug($debug, 'File '.$last_file.' exists');
         $s = file_get_contents($last_file);
         $matches = array();
-        if (preg_match('/x::(\d*)::x/',$s,$matches)) {
+        if (preg_match('/x::(\d*)::x/', $s, $matches)) {
             //enter in panic mode if last execution time was more than 10' ago
             $failing = $failing || ($now>$matches[1]+(60*10));
+            if ($failing) {
+                logOnDebug($debug, '...and it cointains a date more than 10\' old: '.$matches[1]);
+            } else {
+                logOnDebug($debug, '...but nothing too old in there');
+            }
         }
     }
 }
 //If no pid file, nothing to worry about...
 //Do the same for the fixes log
 if (is_file($pid_fix_file)) {
+    logOnDebug($debug, 'File '.$pid_fix_file.' exists (running?)');
     //read contents of last transaction run log
     if (is_file($last_fix_file)) {
+        logOnDebug($debug, 'File '.$pid_fix_file.' exists');
         $s = file_get_contents($last_fix_file);
         $matches = array();
         if (preg_match('/x::(\d*)::x/',$s,$matches)) {
             //enter in panic mode if last execution time was more than 10' ago
             $failing = $failing || ($now>$matches[1]+(60*10));
+            if ($failing) {
+                logOnDebug($debug, '...and it cointains a date more than 10\' old: '.$matches[1]);
+            } else {
+                logOnDebug($debug, '...but nothing too old in there');
+            }
         }
     }
 }
@@ -46,7 +61,7 @@ if (is_file($pid_fix_file)) {
  * React
  */
 if (!$failing) {
-    error_log('Checked locked files. No locked process found. All good.');
+    logOnDebug($debug, 'Checked locked files. No locked process found. All good.');
 } else {
     // if it failed, then we need to:
     echo "Sending mail to admins\n";
@@ -60,15 +75,32 @@ if (!$failing) {
     if (file_exists($pid_file)) {
         $pid = @file_get_contents($pid_file);
         if (!file_exists('/proc/'.trim($pid))) {
+            logOnDebug($debug, 'Trying to delete '.$pid_file.' because pid '.trim($pid).' not present');
             @unlink($pid_file);
+            if (file_exists($pid_file)) {
+                logOnDebug($debug, 'Something prevented file '.$pid_file.' to be deleted!?');
+            }
         }
     }
     if (file_exists($pid_fix_file)) {
         $pid = @file_get_contents($pid_fix_file);
         if (!file_exists('/proc/'.trim($pid))) {
+            logOnDebug($debug, 'Trying to delete '.$pid_fix_file.' because pid '.trim($pid).' not present');
             @unlink($pid_fix_file);
+            if (file_exists($pid_fix_file)) {
+                logOnDebug($debug, 'Something prevented file '.$pid_fix_file.' to be deleted!?');
+            }
         }
     }
     // Actually send the e-mail
+    logOnDebug($debug, 'Trying to send mail alert');
     @api_send_mail($alert_mail,$t,$b);
+    logOnDebug($debug, 'Mail should have been sent');
+}
+
+function logOnDebug($debug, $msg = '')
+{
+    if ($debug) {
+        error_log($msg);
+    }
 }

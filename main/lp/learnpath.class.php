3b3e3c8c2f58c2b525e39e4447a9da93e7b99f25
Yannick Warnier <ywarnier@beeznest.org>
2017-12-28 15:05:50 -0500

    Merge

--- main/lp/learnpath.class.php
+++ main/lp/learnpath.class.php
@@ -11436,7 +11436,7 @@ EOD;
         }
 
         if ($protocolFixApplied == false) {
-            if (strpos($src, api_get_path(WEB_CODE_PATH)) === false) {
+            if (strpos(api_get_path(WEB_PATH), $host) === false) {
                 // Check X-Frame-Options
                 $ch = curl_init();
 

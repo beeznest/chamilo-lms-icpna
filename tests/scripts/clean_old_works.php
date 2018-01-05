6889dcb935b33f559668bbb974566432ac1c71db
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-01-05 15:01:41 -0500

    Delete old remove task script + fix new clean tasks script - refs BT#13832

--- tests/scripts/clean_old_works.php
+++ tests/scripts/clean_old_works.php
@@ -13,67 +13,59 @@
 die();
 require __DIR__.'/../../main/inc/global.inc.php';
 $fromDate = '2017-03-01'; //session start date must be > to be considered
-$expiryDate = '2017-05-01'; //session end date must be < to be considered
+$expiryDate = '2018-01-01'; //session end date must be < to be considered
 $simulate = false;
 if (PHP_SAPI !== 'cli') {
     die('This script can only be executed from the command line');
 }
-echo PHP_EOL . "Usage: php ".basename(__FILE__)." [options]" . PHP_EOL;
-echo "Where [options] can be " . PHP_EOL;
-echo "  -s         Simulate execution - Do not delete anything, just show numbers" . PHP_EOL . PHP_EOL;
-echo "Processing..." . PHP_EOL . PHP_EOL;
+echo PHP_EOL."Usage: php ".basename(__FILE__)." [options]".PHP_EOL;
+echo "Where [options] can be ".PHP_EOL;
+echo "  -s         Simulate execution - Do not delete anything, just show numbers".PHP_EOL.PHP_EOL;
+echo "Processing...".PHP_EOL.PHP_EOL;
 
-require __DIR__.'/../../main/inc/conf/configuration.php';
 if (!empty($argv[1]) && $argv[1] == '-s') {
     $simulate = true;
-    echo "Simulation mode is enabled" . PHP_EOL;
+    echo "Simulation mode is enabled".PHP_EOL;
 }
 
 $sessionCourses = array();
 $coursesCodes = array();
 $coursesDirs = array();
-if (!$conexion = mysql_connect($_configuration['db_host'], $_configuration['db_user'], $_configuration['db_password'])) {
-    echo 'Could not connect to database';
-    exit;
-}
-
-if (!mysql_select_db($_configuration['main_database'], $conexion)) {
-    echo 'Could not select database';
-    exit;
-}
 echo "[".time()."] Querying sessions\n";
 $sql = "SELECT id FROM session where access_start_date < '$expiryDate' AND access_start_date > '$fromDate'";
 
-$res = mysql_query($sql, $conexion);
+$res = Database::query($sql);
+
 if ($res === false) {
-    die("Error querying sessions: ".Database::error($res)."\n");
+    die("Error querying sessions\n");
 }
 
-$countSessions = mysql_num_rows($res);
-$sql = "SELECT count(*) FROM session";
-$resc = mysql_query($sql, $conexion);
+$countSessions = Database::num_rows($res);
+$sql = "SELECT count(*) nbr FROM session";
+$resc = Database::query($sql);
 if ($resc === false) {
-    die("Error querying total sessions count: ".Database::error($res)."\n");
+    die("Error querying total sessions\n");
 }
-$countAllSessions = mysql_result($resc, 0, 0);
-echo "[".time()."] Found $countSessions sessions between $fromDate and $expiryDate on a total of $countAllSessions sessions."."\n";
+$countAllSessions = Database::result($resc, 0, 'nbr');
+echo "[".time()."]"
+    ." Found $countSessions sessions between $fromDate and $expiryDate on a total of $countAllSessions sessions."."\n";
 
-while ($session = mysql_fetch_assoc($res)) {
-    $sql2 = "SELECT c.id AS cid, c.code as ccode, c.directory as cdir
+while ($session = Database::fetch_assoc($res)) {
+    $sql2 = "SELECT c.id AS cid, c.code AS ccode, c.directory AS cdir
             FROM course c, session_rel_course s 
-            WHERE s.id_session = ".$session['id']." 
-            AND s.course_code = c.code";
-    $res2 = mysql_query($sql2, $conexion); //Database::query($sql2);
-    
+            WHERE s.session_id = ".$session['id']." 
+            AND s.c_id = c.id";
+    $res2 = Database::query($sql2);
+
     if ($res2 === false) {
-        die("Error querying courses for session ".$session['id'].": ".mysql_error($res2)."\n");
+        die("Error querying courses for session ".$session['id']."\n");
     }
 
-    if (mysql_num_rows($res2) > 0) {
-        while ($course = mysql_fetch_assoc($res2)) {
+    if (Database::num_rows($res2) > 0) {
+        while ($course = Database::fetch_assoc($res2)) {
             $sessionCourses[$session['id']] = $course['cid'];
-			//$_SESSION['session_course'] = $sessionCourses;
-			
+            //$_SESSION['session_course'] = $sessionCourses;
+
             if (empty($coursesCodes[$course['cid']])) {
                 $coursesCodes[$course['cid']] = $course['ccode'];
             }
@@ -81,19 +73,19 @@ while ($session = mysql_fetch_assoc($res)) {
                 $coursesDirs[$course['cid']] = $course['cdir'];
             }
         }
-    } 
+    }
 }
 echo "[".time()."] Filled courses arrays. Now checking tasks...\n";
 /**
  * Locate and destroy the expired tasks
- */ 
+ */
 //$sessionCourse = $_SESSION['session_course'];
 
 $totalSize = 0;
 foreach ($sessionCourses as $sid => $cid) {
     // Check if a folder already exists in this session
-    // Folders are exclusive to sessions. If a folder already exists in 
-    // another session, you will not be allowed to create the same folder in 
+    // Folders are exclusive to sessions. If a folder already exists in
+    // another session, you will not be allowed to create the same folder in
     // another session. As such, folders belong to one and only one session.
     $sql = "SELECT id, url FROM c_student_publication
             WHERE filetype = 'folder'
@@ -101,14 +93,14 @@ foreach ($sessionCourses as $sid => $cid) {
             AND session_id = $sid
             AND active = 1
             AND url LIKE '%ALP%'";
-	
-    $resCarpetas = mysql_query($sql, $conexion); //Database::query($sql);
 
-    if (mysql_num_rows($resCarpetas) > 0) {
-        while ($rowDemo = mysql_fetch_assoc($resCarpetas)) {
+    $resCarpetas = Database::query($sql);
 
-            $carpetaAlpElimina = $_configuration['root_sys'].'courses/'.$coursesDirs[$cid].'/work'.$rowDemo['url'];
-            
+    if (Database::num_rows($resCarpetas) > 0) {
+        while ($rowDemo = Database::fetch_assoc($resCarpetas)) {
+
+            $carpetaAlpElimina = api_get_path(SYS_COURSE_PATH).$coursesDirs[$cid].'/work'.$rowDemo['url'];
+
             //echo "rm -rf ".$carpetaAlpElimina."\n";
             $size = folderSize($carpetaAlpElimina);
             $totalSize += $size;
@@ -117,22 +109,28 @@ foreach ($sessionCourses as $sid => $cid) {
                 exec('rm -rf '.$carpetaAlpElimina);
             }
         }
-		
-	$sqldel = "DELETE FROM c_student_publication
-		WHERE
-		c_id = $cid
-		AND session_id = $sid AND active = 1;";
-        if ($simulate == false) {
-            $resdel = mysql_query($sqldel);
-        }
-        if ($resdel === false) {
-            echo "Error querying sessions: ".Database::error($resdel)."\n";
-        }
-    } 
-}
-echo "[".time()."] ".($simulate ? "Would delete" : "Deleted")." tasks from $countSessions sessions between $fromDate and $expiryDate on a total of $countAllSessions sessions for a total estimated size of " . round($totalSize/(1024*1024)) . " MB."."\n";
 
-function folderSize($dir) {
+        if ($simulate == false) {
+            $sqldel = "
+                DELETE FROM c_student_publication
+                WHERE c_id = $cid
+                AND session_id = $sid AND active = 1;
+            ";
+            $resdel = Database::query($sqldel);
+
+            if ($resdel === false) {
+                echo "Error querying sessions\n";
+            }
+        }
+    }
+}
+echo "[".time()."] ".($simulate ? "Would delete" : "Deleted")
+    ." tasks from $countSessions sessions between $fromDate and $expiryDate on a total of $countAllSessions"
+    ." sessions for a total estimated size of "
+    .round($totalSize / (1024 * 1024))." MB."."\n";
+
+function folderSize($dir)
+{
     $size = 0;
     $contents = glob(rtrim($dir, '/').'/*', GLOB_NOSORT);
     foreach ($contents as $contents_value) {
@@ -142,5 +140,6 @@ function folderSize($dir) {
             $size += folderSize($contents_value);
         }
     }
+
     return $size;
 }

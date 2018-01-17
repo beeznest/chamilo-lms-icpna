6e19113507c67073eac90c167dbfdde7782e75b8
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-01-17 18:34:57 -0500

    Fix course_chat_restrict_to_coach - refs BT#13885
    Tutors only can chat with students when course_chat_restrict_to_coach is enabled

--- main/inc/lib/CourseChatUtils.php
+++ main/inc/lib/CourseChatUtils.php
@@ -58,6 +58,10 @@ class CourseChatUtils
                     $criteria->andWhere(
                         Criteria::expr()->eq('status', Session::STUDENT)
                     );
+                } else {
+                    $criteria->andWhere(
+                        Criteria::expr()->eq('status', Session::COACH)
+                    );
                 }
             }
 

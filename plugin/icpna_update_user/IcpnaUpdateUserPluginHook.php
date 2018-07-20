be14b3acd972aa7f028dd2fc2030d803fe8e45d4
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-07-20 10:41:27 -0500

    Send back the user name by webservice - refs BT#14666

--- plugin/icpna_update_user/IcpnaUpdateUserPluginHook.php
+++ plugin/icpna_update_user/IcpnaUpdateUserPluginHook.php
@@ -1,6 +1,8 @@
 <?php
 /* For licensing terms, see /license.txt */
 
+use Chamilo\UserBundle\Entity\User;
+
 class IcpnaUpdateUserPluginHook extends HookObserver implements HookUpdateUserObserverInterface
 {
 
@@ -19,72 +21,138 @@ class IcpnaUpdateUserPluginHook extends HookObserver implements HookUpdateUserOb
     }
 
     /**
+     * @param User  $user
+     * @param array $info
+     *
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    private function updateUserInfo(User $user, array $info)
+    {
+        $em = Database::getManager();
+
+        $user
+            ->setFirstname($info['firstname'])
+            ->setLastname($info['lastname']);
+
+        $em->persist($user);
+        $em->flush();
+    }
+
+    /**
+     * @param User  $user
+     * @param array $origExtraInfo
+     * @param array $newInfo
+     */
+    private function updateUserExtraInfo(User $user, array $origExtraInfo, array $newInfo)
+    {
+        $userData = array_merge(
+            $origExtraInfo,
+            [
+                'item_id' => $user->getId(),
+                'extra_middle_name' => $newInfo['extra_middle_name'],
+                'extra_mothers_name' => $newInfo['extra_mothers_name'],
+            ]
+        );
+
+        $extraField = new ExtraFieldValue('user');
+        $extraField->saveFieldValues($userData);
+    }
+
+    /**
+     * @param User $user
+     */
+    private function updateSessionUserInfo(User $user)
+    {
+        $_u = array_merge(
+            ChamiloSession::read('_user', []),
+            [
+                'firstname' => $user->getFirstname(),
+                'lastname' => $user->getLastname(),
+            ]
+        );
+
+        ChamiloSession::write('_user', $_u);
+    }
+
+    /**
      * @param \HookUpdateUserEventInterface $hook
-     * @return array
+     *
+     * @return bool
+     * @throws \Doctrine\ORM\OptimisticLockException
      */
     public function hookUpdateUser(HookUpdateUserEventInterface $hook)
     {
         $data = $hook->getEventData();
 
         if ($data['type'] !== HOOK_EVENT_TYPE_POST) {
-            return $data;
+            return false;
         }
 
         $wsUrl = self::$plugin->get(IcpnaUpdateUserPlugin::SETTING_WEB_SERVICE);
 
         if (empty($wsUrl)) {
-            return [];
-        }
-
-        $userInfo = api_get_user_info();
-        $extraData = UserManager::get_extra_user_data($userInfo['id']);
-
-        if (!isset($extraData['uididpersona'])) {
             return false;
         }
 
-        $studyCenter = $extraData['occupation_center_name_1'];
+        $user = api_get_user_entity(api_get_user_id());
+        $extraData = UserManager::get_extra_user_data($user->getId(), true);
 
-        if ($extraData['occupation_center_name_2']) {
-            $studyCenter = $extraData['occupation_center_name_2'];
+        if (!isset($extraData['extra_uididpersona'])) {
+            return false;
         }
 
-        if ($extraData['occupation_center_name_3']) {
-            $studyCenter = $extraData['occupation_center_name_3'];
+        $wsUserInfo = IcpnaUpdateUserPlugin::create()->getUserInfo($extraData['extra_uididpersona']);
+
+        $this->updateUserInfo($user, $wsUserInfo);
+        $this->updateUserExtraInfo($user, $extraData, $wsUserInfo);
+        $this->updateSessionUserInfo($user);
+
+        $studyCenter = $extraData['extra_occupation_center_name_1'];
+
+        if ($extraData['extra_occupation_center_name_2']) {
+            $studyCenter = $extraData['extra_occupation_center_name_2'];
+        }
+
+        if ($extraData['extra_occupation_center_name_3']) {
+            $studyCenter = $extraData['extra_occupation_center_name_3'];
         }
 
         try {
             $client = new SoapClient($wsUrl);
             $result = $client
                 ->actualizadatospersonales([
-                    'p_uididpersona' => strtoupper($extraData['uididpersona']),
-                    'p_uididdocumentoidentidad' => strtoupper($extraData['id_document_type']),
-                    'p_vchDocumentoNumero' => $extraData['id_document_number'],
-                    'p_chrSexo' => $extraData['sex'],
-                    'p_sdtFechaNacimiento' => $extraData['birthdate'],
-                    'p_uididpaisorigen' => strtoupper($extraData['nationality']),
-                    'p_uidIdDepartamento' => strtoupper($extraData['address_department']),
-                    'p_uidIdProvincia' => strtoupper($extraData['address_province']),
-                    'p_uidIdDistrito' => strtoupper($extraData['address_district']),
-                    'p_vchDireccionPersona' => $extraData['address'],
-                    'p_vchEmailPersona' => $userInfo['email'],
-                    'p_vchTelefonoPersona' => $userInfo['phone'],
-                    'p_vchcelularPersona' => $extraData['mobile_phone_number'],
-                    'p_uidIdOcupacion' => strtoupper($extraData['occupation']),
-                    'p_uididdepartamentocentroestudios' => strtoupper($extraData['occupation_department']),
-                    'p_uididprovinciacentroestudios' => strtoupper($extraData['occupation_province']),
-                    'p_uididdistritocentroestudios' => strtoupper($extraData['occupation_district']),
+                    'p_uididpersona' => strtoupper($extraData['extra_uididpersona']),
+                    'p_uididdocumentoidentidad' => strtoupper($extraData['extra_id_document_type']),
+                    'p_vchDocumentoNumero' => $extraData['extra_id_document_number'],
+                    'p_vchPrimerNombre' => $user->getFirstname(),
+                    'p_vchSegundoNombre' => $wsUserInfo['extra_middle_name'],
+                    'p_vchPaterno' => $user->getLastname(),
+                    'p_vchMaterno' => $wsUserInfo['extra_mothers_name'],
+                    'p_chrSexo' => $extraData['extra_sex'],
+                    'p_sdtFechaNacimiento' => $extraData['extra_birthdate'],
+                    'p_uididpaisorigen' => strtoupper($extraData['extra_nationality']),
+                    'p_uidIdDepartamento' => strtoupper($extraData['extra_address_department']),
+                    'p_uidIdProvincia' => strtoupper($extraData['extra_address_province']),
+                    'p_uidIdDistrito' => strtoupper($extraData['extra_address_district']),
+                    'p_vchDireccionPersona' => $extraData['extra_address'],
+                    'p_vchEmailPersona' => $user->getEmail(),
+                    'p_vchTelefonoPersona' => $user->getPhone(),
+                    'p_vchcelularPersona' => $extraData['extra_mobile_phone_number'],
+                    'p_uidIdOcupacion' => strtoupper($extraData['extra_occupation']),
+                    'p_uididdepartamentocentroestudios' => strtoupper($extraData['extra_occupation_department']),
+                    'p_uididprovinciacentroestudios' => strtoupper($extraData['extra_occupation_province']),
+                    'p_uididdistritocentroestudios' => strtoupper($extraData['extra_occupation_district']),
                     'p_uidIdCentroEstudios' => strtoupper($studyCenter),
-                    'p_vchcentrolaboral' => $extraData['occupation_center_name_4'],
-                    'p_uididcarrerauniversitaria' => strtoupper($extraData['university_career']),
-                    'p_strNombrePadre' => $extraData['guardian_name'],
-                    'p_vchEmailApoderado' => $extraData['guardian_email'],
-                    'p_uidIdDocumentoIdentidadPadre' => strtoupper($extraData['guardian_id_document_type']),
-                    'p_vchDocumentoNumeroPadre' => $extraData['guardian_id_document_number'],
-                    'p_vchNombreUrbanizacion' => $extraData['urbanization'],
-                    'p_uidIdTipoVia' => $extraData['type_of_road'],
-                    'p_chrNroPuerta' => $extraData['door_number'],
-                    'p_chrNroInterior' => $extraData['indoor_number'],
+                    'p_vchcentrolaboral' => $extraData['extra_occupation_center_name_4'],
+                    'p_uididcarrerauniversitaria' => strtoupper($extraData['extra_university_career']),
+                    'p_strNombrePadre' => $extraData['extra_guardian_name'],
+                    'p_vchEmailApoderado' => $extraData['extra_guardian_email'],
+                    'p_uidIdDocumentoIdentidadPadre' => strtoupper($extraData['extra_guardian_id_document_type']),
+                    'p_vchDocumentoNumeroPadre' => $extraData['extra_guardian_id_document_number'],
+                    'p_vchNombreUrbanizacion' => $extraData['extra_urbanization'],
+                    'p_uidIdTipoVia' => $extraData['extra_type_of_road'],
+                    'p_chrNroPuerta' => $extraData['extra_door_number'],
+                    'p_chrNroInterior' => $extraData['extra_indoor_number'],
                 ])
                 ->actualizadatospersonalesResult
                 ->any;
@@ -105,7 +173,5 @@ class IcpnaUpdateUserPluginHook extends HookObserver implements HookUpdateUserOb
 
             return false;
         }
-
-        return $data;
     }
 }

60a9b3c85986f506ef294ec1da389863b55b7645
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-12-17 10:06:21 -0500

    Minor - Improve icpna_gamification languages - refs BT#1544

--- plugin/icpna_gamification/lang/english.php
+++ plugin/icpna_gamification/lang/english.php
@@ -2,24 +2,24 @@
 /* For licensing terms, see /license.txt */
 
 $strings['plugin_title'] = 'ICPNA Gamification';
-$strings['plugin_comment'] = 'Color and progressive display of badges that indicate the student\'s progress in two specific areas: attendance and activities';
+$strings['plugin_comment'] = 'Color and progressive display of badges that indicate the student\'s progress in two specific areas: attendance and activities.';
 
 $strings['active'] = 'Activate';
 $strings['ws_url'] = 'URL for web service';
 
 $strings['LevelUp'] = 'Level Up';
-$strings['ModuleAvailableForStudentsOnly'] = 'This module is available for students only';
-$strings['ModuleNotAvailableForThisCourse'] = 'This module is not available for this course';
-$strings['LevelUpDescription'] = 'With Level Up you will see your effort reflected';
+$strings['ModuleAvailableForStudentsOnly'] = 'This module is available for students only.';
+$strings['ModuleNotAvailableForThisCourse'] = 'This module is not available for this course.';
+$strings['LevelUpDescription'] = 'With Level Up you will see your effort reflected.';
 
 $strings['Attendance'] = 'Attendance';
 
-$strings['AttendanceText080'] = 'Es importante que asistas a tus clases para que logres cumplir tu meta en el idioma inglés. Para nosotros tu aprendizaje es muy importante.';
-$strings['AttendanceText8199'] = '¡Sigue asistiendo puntualmente a tus clases y obtén un 100% de asistencia! Para nosotros tu aprendizaje es muy importante. ¡Pronto premiaremos tu esfuerzo!';
-$strings['AttendanceText100'] = '¡Felicitaciones por tu esfuerzo y constancia! Para nosotros tu aprendizaje es muy importante. ¡Pronto premiaremos tu esfuerzo!';
+$strings['AttendanceText080'] = 'It is important that you attend your classes so that you can achieve your goal in the English language. For us your learning is very important.';
+$strings['AttendanceText8199'] = 'Keep attending your classes on time and get 100% attendance! For us your learning is very important. Soon we will reward your effort!';
+$strings['AttendanceText100'] = 'Congratulations for your effort and perseverance! For us your learning is very important. Soon we will reward your effort!';
 
 $strings['Activities'] = 'Activities';
 
-$strings['ActivitiesText060'] = 'Para nosotros tu aprendizaje es muy importante. Mientras más practiques, mejores habilidades en el idioma tendrás. ¡Sigue esforzándote!';
-$strings['ActivitiesText6180'] = '¡Sigue esforzándote y obtén un 100%! Para nosotros tu aprendizaje es muy importante. ¡Pronto premiaremos tu esfuerzo!';
-$strings['ActivitiesText81100'] = '¡Felicitaciones! Vemos que estás haciendo uso de la plataforma virtual. Estamos orgullosos de tu compromiso. Para nosotros tu aprendizaje es muy importante. ¡Pronto premiaremos tu esfuerzo!';
+$strings['ActivitiesText060'] = 'For us your learning is very important. The more you practice, the better language skills you will have. Keep working!';
+$strings['ActivitiesText6180'] = 'Keep trying and get 100%! For us your learning is very important. Soon we will reward your effort!';
+$strings['ActivitiesText81100'] = 'Congratulations! We see that you are making use of the virtual platform. We are proud of your commitment. For us your learning is very important. Soon we will reward your effort!';
---
+++ plugin/icpna_gamification/lang/spanish.php
@@ -0,0 +1,25 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+$strings['plugin_title'] = 'ICPNA Gamification';
+$strings['plugin_comment'] = 'Despliegue a color y progresivo de distintivos que señalen el avance del estudiante en dos rubros específicos: asistencia y actividades';
+
+$strings['active'] = 'Activate';
+$strings['ws_url'] = 'URL para web service';
+
+$strings['LevelUp'] = 'Level Up';
+$strings['ModuleAvailableForStudentsOnly'] = 'Este módulo sólo está disponible para estudiantes.';
+$strings['ModuleNotAvailableForThisCourse'] = 'Este módulo no está disponible para este curso';
+$strings['LevelUpDescription'] = 'Con Level Up verás reflejado tu esfuerzo';
+
+$strings['Attendance'] = 'Asistencia';
+
+$strings['AttendanceText080'] = 'Es importante que asistas a tus clases para que logres cumplir tu meta en el idioma inglés. Para nosotros tu aprendizaje es muy importante.';
+$strings['AttendanceText8199'] = '¡Sigue asistiendo puntualmente a tus clases y obtén un 100% de asistencia! Para nosotros tu aprendizaje es muy importante. ¡Pronto premiaremos tu esfuerzo!';
+$strings['AttendanceText100'] = '¡Felicitaciones por tu esfuerzo y constancia! Para nosotros tu aprendizaje es muy importante. ¡Pronto premiaremos tu esfuerzo!';
+
+$strings['Activities'] = 'Actividades';
+
+$strings['ActivitiesText060'] = 'Para nosotros tu aprendizaje es muy importante. Mientras más practiques, mejores habilidades en el idioma tendrás. ¡Sigue esforzándote!';
+$strings['ActivitiesText6180'] = '¡Sigue esforzándote y obtén un 100%! Para nosotros tu aprendizaje es muy importante. ¡Pronto premiaremos tu esfuerzo!';
+$strings['ActivitiesText81100'] = '¡Felicitaciones! Vemos que estás haciendo uso de la plataforma virtual. Estamos orgullosos de tu compromiso. Para nosotros tu aprendizaje es muy importante. ¡Pronto premiaremos tu esfuerzo!';

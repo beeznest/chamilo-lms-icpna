88d1156f6f1e3ecd5416edb4aba426e62b705093
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-08-11 17:48:39 -0500

    Quiz: Adaptive: Don't filter message in error result - refs BT#16791

--- main/exercise/exercise_result.php
+++ main/exercise/exercise_result.php
@@ -245,7 +245,7 @@ if ($isAdaptive) {
 
     if ($achievedLevelIsPreTest) {
         Display::addFlash(
-            Display::return_message(get_lang('AdaptiveQuizResultIsPreTestCategory'), 'warning')
+            Display::return_message(get_lang('AdaptiveQuizResultIsPreTestCategory'), 'warning', false)
         );
     } else {
         $quizzesDir = ExerciseLib::checkQuizzesPath($user->getId());

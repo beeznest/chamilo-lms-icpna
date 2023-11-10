d4c9039d64c6f6b4750b875be34a51c17160c012
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-11-10 10:30:58 -0500

    Plugin: ExerciseFocused: Fix missing EntityRepository::countBy method - refs BT#21074

--- plugin/exercisefocused/src/Repository/LogRepository.php
+++ plugin/exercisefocused/src/Repository/LogRepository.php
@@ -5,24 +5,40 @@
 namespace Chamilo\PluginBundle\ExerciseFocused\Repository;
 
 use Chamilo\CoreBundle\Entity\TrackEExercises;
+use Database;
 use Doctrine\ORM\EntityRepository;
 
 class LogRepository extends EntityRepository
 {
     public function countByActionInExe(TrackEExercises $exe, string $action): int
     {
-        return $this->count([
-            'exe' => $exe,
-            'action' => $action,
-        ]);
+        $result = Database::select(
+            'count(id) n',
+            'plugin_exercisefocused_log',
+            [
+                'where' => [
+                    'exe_id = ? AND action = ?' => [$exe->getExeId(), $action]
+                ]
+            ],
+            'first'
+        );
+
+        return (int) $result['n'];
     }
 
     public function countByActionAndLevel(TrackEExercises $exe, string $action, int $level): int
     {
-        return $this->count([
-            'exe' => $exe,
-            'action' => $action,
-            'level' => $level,
-        ]);
+        $result = Database::select(
+            'count(id) n',
+            'plugin_exercisefocused_log',
+            [
+                'where' => [
+                    'exe_id = ? AND action = ? AND level = ?' => [$exe->getExeId(), $action, $level]
+                ]
+            ],
+            'first'
+        );
+
+        return (int) $result['n'];
     }
 }

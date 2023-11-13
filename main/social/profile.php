chamilo/chamilo-lms

Social: Security token for chat and social wall

main/social/profile.php
@@ -141,7 +141,7 @@ $sessionList = [];
 
 // My friends
 $friend_html = SocialManager::listMyFriendsBlock($user_id, $link_shared);
-$addPostForm = SocialManager::getWallForm(api_get_self());
+$addPostForm = SocialManager::displayWallForm(api_get_self());
 $addPostFormPortfolio = SocialManager::getWallFormPortfolio(api_get_self());
 
 $posts = SocialManager::getWallMessagesByUser($friendId);

beeznest/chamilo-lms-icpna

main/social/profile.php
@@ -173,7 +173,7 @@ $sessionList = SessionManager::getSessionsFollowedByUser(
 
 // My friends
 $friend_html = SocialManager::listMyFriendsBlock($user_id, $link_shared);
-$wallSocialAddPost = SocialManager::getWallForm(api_get_self());
+$wallSocialAddPost = SocialManager::displayWallForm(api_get_self());
 
 $posts = SocialManager::getWallMessagesByUser($friendId);
 $socialAutoExtendLink = SocialManager::getAutoExtendLink($user_id, $countPost);
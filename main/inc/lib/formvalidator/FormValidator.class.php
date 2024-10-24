3bd96bd77f7c4b08a94d7e239685e6b24e7fe8b6
Yannick Warnier <ywarnier@beeznest.org>
2024-10-24 12:34:43 +0200

    Global: Fix df47eac9b93700bdf3a73e2596e956e14ab1e4f2 where the filter for "on" attributes was too wide and replaced normal text containing " on" - refs #5887

--- main/inc/lib/formvalidator/FormValidator.class.php
+++ main/inc/lib/formvalidator/FormValidator.class.php
@@ -1850,5 +1850,5 @@ function mobile_phone_number_filter($mobilePhoneNumber)
 function attr_on_filter($html) {
     $prefix = uniqid('data-cke-').'-';
 
-    return preg_replace('/(\s)(on)/i', '$1'.$prefix.'$2', $html);
+    return preg_replace('/\b(on[a-z]+)\b\s*=/i', '$1'.$prefix.'$2', $html);
 }

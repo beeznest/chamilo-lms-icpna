chamilo/chamilo-lms

Plugin: AzureActiveDirectory: Fix error when loading index file as standalone

plugin/azure_active_directory/index.php
@@ -6,29 +6,33 @@
  * @package chamilo.plugin.azure_active_directory
  */
 
-/** @var AzureActiveDirectory $activeDirectoryPlugin */
-$activeDirectoryPlugin = AzureActiveDirectory::create();
+// Check if AzureActiveDirectory exists, since this is not loaded as a page.
+// index.php is shown as a block when showing the region to which the plugin is assigned
+if (class_exists(AzureActiveDirectory::class)) {
+    /** @var AzureActiveDirectory $activeDirectoryPlugin */
+    $activeDirectoryPlugin = AzureActiveDirectory::create();
 
-if ($activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_ENABLE) === 'true') {
-    $_template['block_title'] = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_BLOCK_NAME);
+    if ($activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_ENABLE) === 'true') {
+        $_template['block_title'] = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_BLOCK_NAME);
 
-    $_template['signin_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_AUTHORIZE);
+        $_template['signin_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_AUTHORIZE);
 
-    if ('true' === $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_FORCE_LOGOUT_BUTTON)) {
-        $_template['signout_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_LOGOUT);
-    }
-
-    $managementLoginEnabled = 'true' === $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_MANAGEMENT_LOGIN_ENABLE);
-
-    $_template['management_login_enabled'] = $managementLoginEnabled;
-
-    if ($managementLoginEnabled) {
-        $managementLoginName = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_MANAGEMENT_LOGIN_NAME);
-
-        if (empty($managementLoginName)) {
-            $managementLoginName = $activeDirectoryPlugin->get_lang('ManagementLogin');
+        if ('true' === $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_FORCE_LOGOUT_BUTTON)) {
+            $_template['signout_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_LOGOUT);
         }
 
-        $_template['management_login_name'] = $managementLoginName;
+        $managementLoginEnabled = 'true' === $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_MANAGEMENT_LOGIN_ENABLE);
+
+        $_template['management_login_enabled'] = $managementLoginEnabled;
+
+        if ($managementLoginEnabled) {
+            $managementLoginName = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_MANAGEMENT_LOGIN_NAME);
+
+            if (empty($managementLoginName)) {
+                $managementLoginName = $activeDirectoryPlugin->get_lang('ManagementLogin');
+            }
+
+            $_template['management_login_name'] = $managementLoginName;
+        }
     }
 }

beeznest/chamilo-lms-icpna

plugin/azure_active_directory/index.php
@@ -6,27 +6,32 @@
  * @package chamilo.plugin.azure_active_directory
  */
 
-/** @var AzureActiveDirectory $activeDirectoryPlugin */
-$activeDirectoryPlugin = AzureActiveDirectory::create();
+// Check if AzureActiveDirectory exists, since this is not loaded as a page.
+// index.php is shown as a block when showing the region to which the plugin is assigned
+if (class_exists(AzureActiveDirectory::class)) {
+    /** @var AzureActiveDirectory $activeDirectoryPlugin */
+    $activeDirectoryPlugin = AzureActiveDirectory::create();
 
-if ($activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_ENABLE) === 'true') {
-    $signUp = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_SIGNIN_POLICY);
-    $signIn = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_SIGNUP_POLICY);
-    $signUnified = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_SIGNUNIFIED_POLICY);
+    if ($activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_ENABLE) === 'true') {
+        $signUp = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_SIGNIN_POLICY);
+        $signIn = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_SIGNUP_POLICY);
+        $signUnified = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_SIGNUNIFIED_POLICY);
 
-    $_template['block_title'] = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_BLOCK_NAME);
+        $_template['block_title'] = $activeDirectoryPlugin->get(AzureActiveDirectory::SETTING_BLOCK_NAME);
 
-    if ($signUp) {
-        $_template['signup_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_SIGNUP);
+        if ($signUp) {
+            $_template['signup_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_SIGNUP);
+        }
+
+        if ($signIn) {
+            $_template['signin_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_SIGNIN);
+        }
+
+        if ($signUnified) {
+            $_template['signunified_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_SIGNUNIFIED);
+        }
+
+        $_template['signout_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_SIGNOUT);
+
     }
-
-    if ($signIn) {
-        $_template['signin_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_SIGNIN);
-    }
-
-    if ($signUnified) {
-        $_template['signunified_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_SIGNUNIFIED);
-    }
-
-    $_template['signout_url'] = $activeDirectoryPlugin->getUrl(AzureActiveDirectory::URL_TYPE_SIGNOUT);
 }